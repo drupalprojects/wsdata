af48d9a61ce2c46854d0218f0d58514deae388ea
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2013-11-24 12:03:10 -0500

    Added some API docs, todos and entity info options

---
+++ modules/wsentities/README.md
@@ -0,0 +1,14 @@
+Web Service Entities
+====================
+
+Defines as entity controller for entity in a web services.
+
+Also includes helper functions for defining new entity types and altering existing entities to load data from a web service instead of the database.
+
+Limitations
+----------
+
+- Non-fieldable entities only
+- Single bundle entities only
+- EntityCache not supported
+- Read-only interface
---
+++ modules/wsentities/wsentities.api.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @file
+ * Sample API documentation for WSEntities
+ */
+
+/**
+ * Define a new remote entity type.
+ */
+function hook_entity_info() {
+  /**
+   * WSEntities can be used with most of the basic options in 
+   * hook_entity_info().
+   *
+   * Note that wsconfig and wsconfig_type entities cannot use the WSEntityAPIController 
+   *
+   * It also introduces a few new options. You can add these to your info array by 
+   * calling wsentities_add_entity_info();
+   *
+   * @see wsentities_add_entity_info().
+   */
+
+  // Add the new options. Will return FALSE if invalid options are set.
+  // If the options were invalid, your info array remains unchanged.
+  $valid = wsentities_add_entity_info($info, 'wsconfig_machine_name');
+
+  $info = array(
+    '...',
+    'controller class' => 'WSEntityAPIController', // Required. Entity controller designed for use with web services.
+    'wsconfig' => 'wsconfig_machine_name', // Required. Name of the wsconfig instance responsible for all data operations for this entity type.
+    'cache-control' => TRUE, // Optional. Whether to respect the cache-control headers returned
+    'cache-control-expiry' => 3600, // Optional. If cache-control is set to FALSE, how long to cache items for.
+    '...',
+  );
+}
+
+/**
+ * Convert an existing entity into a remote entity
+ */
+function hook_entity_info_alter(&$info) {
+  // You can apply the options using an alter as well
+  $valid = wsentities_add_entity_info($info, 'wsconfig_machine_name');
+}
